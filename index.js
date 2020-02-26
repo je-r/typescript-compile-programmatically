8014604d17ce57e4e57a76058c6170b49a382ed2
J.E. R <je-r@users.noreply.github.com>
2020-02-26 19:33:06 +0100

    dev

---
+++ index.js
@@ -0,0 +1,19 @@
+//import * as ts from "typescript";
+
+var globalThis = require('globalthis')();
+
+
+var ts = require('typescript');
+
+const source = 
+ ` let x: string  = 'some string';
+   console.log("x=["+x+"]")
+ `;
+
+let result = ts.transpileModule(source, { compilerOptions: { module: ts.ModuleKind.CommonJS }});
+
+console.log(JSON.stringify(result));
+
+console.log("———————-")
+
+eval(result["outputText"]);
